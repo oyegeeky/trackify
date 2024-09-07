52456ab85f7dadd3d4ff26602cc8890ae28bc9b0
oyegeeky <ayushmaan.sural.csbs24@heritageit.edu.in>
2024-09-08 03:34:30 +0530

    Commit check

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Text, View, StyleSheet } from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello, World!</Text>
+      <Text style={styles.text}>Simplify Spends with Trackify </Text>
     </View>
   );
 };
